fbd5dea8d9f2d1c87fc2d00275ada8ce4030a01f
Dale Willemse <DaleWillemse000@gmail.com>
2023-05-07 21:03:51 +0200

    runtime 144ms

---
+++ Easy/RemoveElement.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        
+    int count = 0;
+
+    foreach(int num in nums){
+        if(num != val){
+            nums[count] = num;
+            count++;
+        }
+    }
+    return count;
+    }
+}
